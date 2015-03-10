2cd9418ee10c40ab0c86541276bc55bea35a40cb
cocoswank <patchkatie@gmail.com>
2015-03-10 09:44:50 -0500

    Styled flash and fixed return link on other-links

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -14,6 +14,5 @@ class HomeController < ApplicationController
         "Good Night! <i class='fa fa-star'></i>".html_safe
       end
       @quote = Quote.all.sample.quip
-      @year = right_now.year
   end
 end
